4c1075addc359836bb049d1945c3eb097a47d358
Johannes Maron <johannes@maron.family>
2026-09-11 19:14:52 +0200

    Add different favicon for local development (#223)

--- root/settings.py
+++ root/settings.py
@@ -169,6 +169,7 @@ TEMPLATES = [
                 "django.template.context_processors.i18n",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "root.context_processors.settings_context",
             ],
             "debug": DEBUG,
             "loaders": (
