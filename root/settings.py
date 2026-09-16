0af831585dfd1b3547997fe345847fc2c7b0a416
Johannes Maron <johannes@maron.family>
2026-09-16 21:56:41 +0200

    Move the storage docs link to the block header
    The link documents the whole block, so it belongs with the header rather
    than on one setting.

--- root/settings.py
+++ root/settings.py
@@ -258,14 +258,13 @@ STORAGES = {
 }
 
 # S3 / django-storages
+# https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
 AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME", default="")
 AWS_S3_REGION_NAME = env("AWS_S3_REGION_NAME", default="us-east-1")
 AWS_S3_ACCESS_KEY_ID = env("AWS_S3_ACCESS_KEY_ID", default="")
 AWS_S3_SECRET_ACCESS_KEY = env("AWS_S3_SECRET_ACCESS_KEY", default="")
 AWS_S3_ENDPOINT_URL = env("AWS_S3_ENDPOINT_URL", default="")
 AWS_S3_ADDRESSING_STYLE = env("AWS_S3_ADDRESSING_STYLE", default="auto")
-# Never replace a stored message body, see
-# https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
 AWS_S3_FILE_OVERWRITE = False
 
 # Static files (CSS, JavaScript, Images)
