1e749f2bcf3dcdc5dd016cc79fdde80b41d29805
Johannes Maron <johannes@maron.family>
2026-09-19 22:28:01 +0200

    Highlight the landing snippets with Pygments

--- root/static/js/start.js
+++ root/static/js/start.js
@@ -1,5 +1,3 @@
-import hljs from "https://esm.sh/highlight.js@11.11.1/lib/common";
-
 const slider = document.getElementById("price-slider");
 const amount = document.getElementById("price-amount");
 const volume = document.getElementById("price-volume");
@@ -28,8 +26,4 @@ if (slider && amount && volume) {
 
   slider.addEventListener("input", update);
   update();
-}
-
-for (const block of document.querySelectorAll("pre code[class*='language-']")) {
-  hljs.highlightElement(block);
 }
